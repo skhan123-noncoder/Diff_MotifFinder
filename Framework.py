5a318d0b6c4c7212029f741c77f62a4a930fd397
skhan123-noncoder <soumenkhan123@gmail.com>
2023-01-27 16:20:38 +0000

    Fixed parse arguments, function calling, variable pushing

--- Framework.py
+++ Framework.py
@@ -1,6 +1,7 @@
 #/home/usr/bin/python
 
-from Bio import Align
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 from Bio import SeqIO
 import os
 import sys
@@ -9,10 +10,9 @@ import argparse
 def main():
      
     user_provided_inputs=parse_arguments()
-    print(user_provided_inputs)
-     #fasta_length_check=quality_check(user_provided_inputs[0], user_provided_inputs[1])
-     #func3=run_alignment()
-     #func4=run_mast()
+    fasta_length_check=quality_check(user_provided_inputs.infile1, user_provided_inputs.infile2)
+    alignment_check=run_alignment(fasta_length_check[0][0],fasta_length_check[0][1])
+    primary_outfile=run_mast(user_provided_inputs.infile1, user_provided_inputs.infile2, user_provided_inputs.TFdatabase, user_provided_inputs.outfile)
      #func4=analyze_mast_output()
 
 def parse_arguments():
@@ -23,49 +23,35 @@ def parse_arguments():
     parser.add_argument('-o', '--output', action='store', dest='outfile', type=str, help="name of the output file", default="out_file")
     args=parser.parse_args()
     path=str(args.outfile)
+    return(args)
 
-    #infile=str(os.path.basename(args.infile))
-    #TFdatabase=str(os.path.basename(args.TFdatabase))
-    #outfile=str(os.path.basename(args.outfile))
-    data=[infile1, infile2, TFdatabase, outfile, path]
-    return(data)
-
-def quality_check():
-    data=parse_arguments()
+def quality_check(fasta1, fasta2):
     seqID=[]
     fasta=[]
-    
-    for record in SeqIO.parse(data[0], "fasta"):
-        seqID.append(record.id)
-        fasta.append(record.seq)
-    #print(seqID)
-    #print(fasta)
+    for record1 in SeqIO.parse(fasta1, "fasta"):
+        seqID.append(record1.id)
+        fasta.append(record1.seq)
+    for record2 in SeqIO.parse(fasta2, "fasta"):
+        seqID.append(record2.id)
+        fasta.append(record2.seq)
     if len(fasta[0])!=len(fasta[1]):
         raise Exception("Sequences need to be of the same length")
     else:
         print("Sequences are of same length. Performing pairwise alignment")
 
-    return(fasta)
+    return(fasta,seqID)
 
-#This function also does a pairwise alignment of the sequnces provided. This gives the user a rough estimation of where the differences lie
-def run_alignment():
-    func=parse_arguments()
-    sys.stdout=open(func[2], 'w')
-    fasta_list=quality_check()
-    aligner=Align.PairwiseAligner()
-    alignment= aligner.align(fasta_list[0], fasta_list[1])
-    for alignments in alignment:
-        Score = alignments.score
-    print(Score, '\n', alignments)
+def run_alignment(fasta_seq1,fasta_seq2):
+    alignment= pairwise2.align.globalxx(fasta_seq1, fasta_seq2)
+    print(format_alignment(*alignment[0]))
 
 #This function calls the meme module and runs the mast function
 ##Unable to load the meme module while running this. Need to have a closer look at this
-def run_mast():
+def run_mast(fasta1,fasta2,TF,outfile):
 
-    argument_list=parse_arguments()
-    os.system('module load meme')
-    os.system('mast -o '+ argument_list[2]+ ' -nostatus -minseqs 1 -remcorr -ev 10.0 ' +argument_list[1]+ ' '+argument_list[0])
-    os.system('mast -hit_list -nostatus -minseqs 1 -remcorr -ev 10.0 HOCOMOCOv11_core_HUMAN_mono_meme_format.meme fasta_test_2seq.txt >'+ argument_list[2]+'/hit_list_temp.txt') 
+    subprocess.run('mast -o '+outfile+ ' -nostatus -minseqs 1 -remcorr -ev 10.0 ' +TF+ ' '+fasta1)
+    subprocess.run('mast -o '+outfile+ ' -nostatus -minseqs 1 -remcorr -ev 10.0 ' +TF+ ' '+fasta2)
+    subprocess.run('mast -hit_list -nostatus -minseqs 1 -remcorr -ev 10.0 HOCOMOCOv11_core_HUMAN_mono_meme_format.meme fasta_test_2seq.txt >'+ argument_list[2]+'/hit_list_temp.txt') 
 
 def analyze_mast_output():
     seqID=[]
