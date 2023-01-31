5682bc8b1ef1bc4d5ca135a28f8dd0c0b55cf87f
skhan123-noncoder <soumenkhan123@gmail.com>
2023-01-31 11:07:54 +0000

    Naming improved, Unused variables removed, Better use of functions

--- Framework.py
+++ Framework.py
@@ -1,19 +1,20 @@
 #/home/usr/bin/python
-
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio import SeqIO
 import os
-import sys
+import subprocess
 import argparse
+import tempfile
+import re
 
 def main():
      
     user_provided_inputs=parse_arguments()
     fasta_length_check=quality_check(user_provided_inputs.infile1, user_provided_inputs.infile2)
-    alignment_check=run_alignment(fasta_length_check[0][0],fasta_length_check[0][1])
-    primary_outfile=run_mast(user_provided_inputs.infile1, user_provided_inputs.infile2, user_provided_inputs.TFdatabase, user_provided_inputs.outfile)
-     #func4=analyze_mast_output()
+    alignment_check=run_alignment(fasta_length_check[0][0],fasta_length_check[0][1],fasta_length_check[3])
+    primary_outfile=run_mast(fasta_length_check[2], user_provided_inputs.TFdatabase, user_provided_inputs.outfile,fasta_length_check[3])
+    func4=analyze_mast_output(primary_outfile,fasta_length_check[1][0],fasta_length_check[1][1],user_provided_inputs.outfile)
 
 def parse_arguments():
     parser=argparse.ArgumentParser(prog='Differential Motif finder', description="Provide fasta file of containing a pair of simialr DNA sequences of equal lengths", epilog='none')
@@ -23,90 +24,106 @@ def parse_arguments():
     parser.add_argument('-o', '--output', action='store', dest='outfile', type=str, help="name of the output file", default="out_file")
     args=parser.parse_args()
     path=str(args.outfile)
+
     return(args)
 
+#Checking for the length of the fasta files. If unequal, the analysis might be different. Needs to have more robustness here
 def quality_check(fasta1, fasta2):
     seqID=[]
     fasta=[]
     for record1 in SeqIO.parse(fasta1, "fasta"):
         seqID.append(record1.id)
-        fasta.append(record1.seq)
+        fasta.append(record1.seq)        
     for record2 in SeqIO.parse(fasta2, "fasta"):
         seqID.append(record2.id)
         fasta.append(record2.seq)
     if len(fasta[0])!=len(fasta[1]):
-        raise Exception("Sequences need to be of the same length")
+        raise Exception("Sequences need to be of the same length. Unequal sequences will have inconsistent results!")
     else:
         print("Sequences are of same length. Performing pairwise alignment")
+    
+    #Creating a temp directory where you store your combined fasta file and store the 
+    temp_dir=tempfile.mkdtemp(dir=os.path.dirname(fasta1))
+    mast_fa_input=open(os.path.abspath(temp_dir)+'/mast_fasta_file','w')
+    with open(fasta1,'r') as f1:
+        for each in f1.readlines():
+            mast_fa_input.write(each)
+        mast_fa_input.write('\n')
+    f1.close()
+    with open(fasta2,'r') as f2:
+        for each in f2.readlines():
+            mast_fa_input.write(each)
+        mast_fa_input.write('\n')
+    f2.close()
+    mast_fa_input.close()
+    
+    return(fasta,seqID,mast_fa_input.name,temp_dir)
 
-    return(fasta,seqID)
+def run_alignment(fasta_seq1,fasta_seq2,temp_dir):
 
-def run_alignment(fasta_seq1,fasta_seq2):
+    aligned_file=open(os.path.abspath(temp_dir)+'/alignment.txt', 'w')
     alignment= pairwise2.align.globalxx(fasta_seq1, fasta_seq2)
-    print(format_alignment(*alignment[0]))
+    aligned_file.write(format_alignment(*alignment[0]))
 
-#This function calls the meme module and runs the mast function
-##Unable to load the meme module while running this. Need to have a closer look at this
-def run_mast(fasta1,fasta2,TF,outfile):
 
-    subprocess.run('mast -o '+outfile+ ' -nostatus -minseqs 1 -remcorr -ev 10.0 ' +TF+ ' '+fasta1)
-    subprocess.run('mast -o '+outfile+ ' -nostatus -minseqs 1 -remcorr -ev 10.0 ' +TF+ ' '+fasta2)
-    subprocess.run('mast -hit_list -nostatus -minseqs 1 -remcorr -ev 10.0 HOCOMOCOv11_core_HUMAN_mono_meme_format.meme fasta_test_2seq.txt >'+ argument_list[2]+'/hit_list_temp.txt') 
+def run_mast(mast_fa_input,TF,outfile,temp_dir):
+    subprocess.run(['mast','-nostatus','-minseqs','1','-remcorr','-ev','10.0','-o',f"{outfile}",TF,mast_fa_input])
+    hit=open(temp_dir+'/hit_list.txt', 'w')
+    subprocess.run(['mast','-hit_list','-nostatus','-minseqs','1','-remcorr','-ev','10.0',TF,mast_fa_input], stdout=hit)
+    hit.close()
+    return(hit.name)
 
-def analyze_mast_output():
-    seqID=[]
-    outfile_location=parse_arguments()
-    sys.stdout=open("Differential_analysis_file.txt", 'w')
-
-    for record in SeqIO.parse(outfile_location[0], "fasta"):
-        seqID.append(record.id)
-
-    with open(outfile_location[2]+'/hit_list_temp.txt', 'r') as file:
-        list1=[]
-        list2=[]
-        for each in file:
+def analyze_mast_output(hit_list,seq1,seq2,outfile):
+    with open(hit_list,'r') as hit_file:
+        comp_list1=[]
+        comp_list2=[]
+        for each in hit_file:
             if re.search('# sequence_name', each):
-                header=each
                 header=re.sub(r'# sequence_name',"sequence_name", each)
-            
-            if not re.search('#', each):
-                if str(each.split()[0])==str(seqID[0]):
-                    list1.append(re.sub(str(seqID[0]), "", each))
-                elif str(each.split()[0])==str(seqID[1]):
-                    list2.append(re.sub(str(seqID[1]), "", each))
-        i=len(list1)
-        j=len(list2)
-        if i == j:
-            print("The two sequences have equal number of TF binding motifs, check further for detailed analysis")
-            diff=0
-            while i>=0:
-                var1=str(list1[i-1])
-                var2=str(list2[i-1])
-                if var1 != var2:
-                    diff=diff+1
-                print('\n')
-                #print(f"################  Difference number {diff} #######################")
-                sys.stdout.write(header)
-                sys.stdout.write(str(seqID[0])+var1)
-                print(str(seqID[1])+var2)
-                if var1.split()[1] != var2.split()[1]:
-                    print("Analysis: There is a difference in binding of the TF {var1.split()[1]} vs {var2.split()[1]} between the two sequences")
-                    print("Analysis: The position where this difference occurs is at the {var1.split()[0]}")
-                    
-                elif var1.split()[1] == var2.split()[1]:
-                    if var1.split()[0] != var2.split()[0]:
-                        print("Analysis: There is no difference in binding of the TF, but the position of binding has shifted from {var1.split()[1]} to {var2.split()[1]} between the two sequences")
-                    if var1.split()[-1] != var2.split()[-1]:
-                        print("Analysis: There is no difference in binding of the TF, but the p-value of binding has changed from {var1.split()[-1]} to {var2.split()[-2]} between the two sequences")
-                    if var1.split()[3] != var2.split()[3] or var1.split()[4] != var2.split()[4]:
-                        print("Analysis: There is no difference in binding of the TF, but the position of binding has chnaged from {var1.split()[3]}&{var1.split()[4]} to {var2.split()[3]}&{var2.split()[4]}  between the two sequences")
-                    
-            i=i-1
+            if each.split()[0]==seq1:
+                comp_list1.append(each)
+            elif each.split()[0]==seq2:
+                comp_list2.append(each)
+    hit_file.close()
 
-        if diff == 0:
-            sys.stdout.write("Sorry, not much difference in your DNA sequences in terms of Motif binding")
-   
-                          
+    analysis=open(os.path.abspath(outfile)+'/analysis_file.txt', 'w')
+
+    if len(comp_list1)==len(comp_list2):
+        analysis.write("The two sequences have equal number of TF binding motifs, check further for detailed analysis")
+    analysis.write('\n')
+            
+    TF_entries=len(comp_list1)
+    while TF_entries>=0:
+        if comp_list1[TF_entries-1].split()[1:]!=comp_list2[TF_entries-1].split()[1:]:
+            if comp_list1[TF_entries-1].split()[2]!=comp_list2[TF_entries-1].split()[2]:
+                analysis.write(header)
+                #analysis.write('\n')
+                analysis.write('%s'%(comp_list1[TF_entries-1]))
+                analysis.write('%s'%(comp_list2[TF_entries-1]))
+                analysis.write('\n')
+                analysis.write('Analysis: There is a difference in binding of the TF %s vs %s between the sequences' %({comp_list1[TF_entries-1].split()[2]},{comp_list2[TF_entries-1].split()[2]}))
+                #analysis.write('\n')
+                analysis.write(f'Analysis: The position where this difference occurs is at the position: %s' %({comp_list1[TF_entries-1].split()[1]}))
+                analysis.write('\n')
+            analysis.write('\n')        
+            if comp_list1[TF_entries-1].split()[2] == comp_list2[TF_entries-1].split()[2]:
+                analysis.write(header)
+                analysis.write('%s'%(comp_list1[TF_entries-1]))
+                analysis.write('%s'%(comp_list2[TF_entries-1]))
+                analysis.write('\n')
+                if comp_list1[TF_entries-1].split()[1] != comp_list2[TF_entries-1].split()[1]:
+                    analysis.write("Analysis: There is no difference in binding of the TF, but the position of binding has shifted from %s to %s between the two sequences" %({comp_list1[TF_entries-1].split()[1]},{comp_list2[TF_entries-1].split()[1]}))
+                    analysis.write('\n')
+                if comp_list1[TF_entries-1].split()[-1] != comp_list2[TF_entries-1].split()[-1]:
+                    analysis.write("Analysis: There is no difference in binding of the TF, but the p-value of binding has changed from %s to %s between the two sequences" %({comp_list1[TF_entries-1].split()[-1]},{comp_list2[TF_entries-1].split()[-2]}))
+                    analysis.write('\n')
+                if comp_list1[TF_entries-1].split()[4] != comp_list2[TF_entries-1].split()[4] or comp_list1[TF_entries-1].split()[5] != comp_list2[TF_entries-1].split()[5]:
+                    analysis.write("Analysis: There is no difference in binding of the TF, but the position of binding has chnaged from %s & %s to %s and %s between the two sequences" %({comp_list1[TF_entries-1].split()[4]},{comp_list1[TF_entries-1].split()[5]},{comp_list2[TF_entries-1].split()[4]},{comp_list2[TF_entries-1].split()[5]}))
+                    analysis.write('\n')
+            analysis.write('\n')
+        TF_entries=TF_entries-1
+
+    analysis.close()                
 
 if __name__=='__main__':
     main()
